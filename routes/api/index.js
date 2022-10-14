df0a78f160c7f071182d4e958a9adea54fea5756
sulemankhan1 <m.sulemankhan@hotmail.com>
2022-10-14 22:35:06 +0500

    completed

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,7 @@
 // creating all end points here in one file just because this is a task
 // project in real project every entity will be in separate folder
+// also i haven't added any validation
+
 const express = require("express");
 const router = express.Router();
 
@@ -34,47 +36,105 @@ router.get("/getAllVenues", async (req, res) => {
   }
 });
 
-//Get all Method
-router.get("/getAll", async (req, res) => {
+// Endpoint: /api/addEmployee
+// Task: Add New Employee
+router.post("/addEmployee", async (req, res) => {
+  const data = new Employee({
+    name: req.body.name,
+    age: req.body.age,
+  });
+
   try {
-    const data = await Model.find();
-    res.json(data);
+    const dataToSave = await data.save();
+    res.status(200).json({ type: "success", data: dataToSave });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(400).json({ message: error.message });
   }
 });
 
-//Get by ID Method
-router.get("/getOne/:id", async (req, res) => {
+// Endpoint: /api/getAllEmployees
+// Returns: List of Employees available
+router.get("/getAllEmployees", async (req, res) => {
   try {
-    const data = await Model.findById(req.params.id);
-    res.json(data);
+    const data = await Employee.find();
+    res.json({ type: "success", data });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ type: "error", message: error.message });
   }
 });
 
-//Update by ID Method
-router.patch("/update/:id", async (req, res) => {
+// Endpoint: /api/book
+// Task: Insert record in EmployeeSlot
+router.post("/book", async (req, res) => {
+  const data = new EmployeeSlot({
+    employeeId: req.body.employeeId,
+    venueId: req.body.venueId,
+    scheduledAt: req.body.scheduledAt,
+    notes: req.body.notes,
+    status: "ALLOCATED",
+  });
+
   try {
-    const id = req.params.id;
-    const updatedData = req.body;
-    const options = { new: true };
-
-    const result = await Model.findByIdAndUpdate(id, updatedData, options);
-
-    res.send(result);
+    const dataToSave = await data.save();
+    res.status(200).json({ type: "success", data: dataToSave });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(400).json({ message: error.message });
   }
 });
 
-//Delete by ID Method
-router.delete("/delete/:id", async (req, res) => {
+// Endpoint: /api/reschedule
+// Task: Reschedule a Slot
+router.patch("/reschedule", async (req, res) => {
   try {
-    const id = req.params.id;
-    const data = await Model.findByIdAndDelete(id);
-    res.send(`Document with ${data.name} has been deleted..`);
+    const record = await EmployeeSlot.findById(req.body.EmployeeSlotId);
+    // record.scheduledAt = req.body.scheduledAt;
+
+    // const dataToSave = await record.save();
+    res.status(200).json({ type: "success", data: record });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Endpoint: /api/cancel
+// Task: Reschedule a Slot
+router.patch("/cancel", async (req, res) => {
+  try {
+    const record = await EmployeeSlot.findById(req.body.EmployeeSlotId);
+    record.status = "CANCELLED";
+
+    const dataToSave = await record.save();
+    res.status(200).json({ type: "success", data: dataToSave });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Endpoint: /api/getEmployeeSlot
+// Returns: returns list from EmployeeSlot table by employee id
+router.get("/getEmployeeSlot", async (req, res) => {
+  try {
+    const data = await EmployeeSlot.find({
+      employeeId: req.body.employeeId,
+      // did the above with req.params.employeeId but for some reason
+      // i was getting error maybe because of postman ...due to short time
+      // shortage i changed it to req.body to make the api work for now
+    });
+    res.json({ type: "success", data });
+  } catch (error) {
+    res.status(500).json({ type: "error", message: error.message });
+  }
+});
+
+// Endpoint: /api/completed
+// Task: complete a slot
+router.patch("/completed", async (req, res) => {
+  try {
+    const record = await EmployeeSlot.findById(req.body.EmployeeSlotId);
+    record.status = "COMPLETE";
+
+    const dataToSave = await record.save();
+    res.status(200).json({ type: "success", data: dataToSave });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
